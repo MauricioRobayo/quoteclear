4371bda0d6a15b12a20550a348d69b2a16eacfc0
Mauricio Robayo <rfmajo@gmail.com>
2021-09-07 10:01:33 -0500

    feat: add header styling
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,9 +1,35 @@
 import React from "react";
-import { ThemeProvider } from "styled-components/macro";
+import styled, { ThemeProvider } from "styled-components/macro";
 import { Normalize } from "styled-normalize";
 import { Quote } from "./components/Quote";
 import usePreferredColorScheme from "./hooks/usePreferredColorScheme";
 import { GlobalStyle, theme } from "./styles";
+
+const Header = styled.header`
+  text-align: center;
+  font-family: ${({ theme }) => theme.fontFamily.text2};
+`;
+
+const Title = styled.h1`
+  letter-spacing: 0.1em;
+  font-size: 1.25rem;
+  font-weight: normal;
+  text-align: center;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  span {
+    letter-spacing: 0;
+    font-weight: normal;
+    font-size: 0.85rem;
+    margin-left: 0.25em;
+  }
+`;
+
+const Flip = styled.span`
+  display: Inline-block;
+  transform: scale(-1, 1);
+`;
 
 function App() {
   const preferredColorScheme = usePreferredColorScheme();
@@ -12,16 +38,20 @@ function App() {
       <ThemeProvider theme={theme[preferredColorScheme]}>
         <Normalize />
         <GlobalStyle />
-        <header>
-          <h1>JAMES CLEAR QUOTES</h1>
+        <Header>
+          <Title>
+            JAMES CLEAR{" "}
+            <span>
+              <Flip>Q</Flip>UOTES
+            </span>
+          </Title>
           <div>
             <p>
               Taken from{" "}
-              <a href="https://jamesclear.com/3-2-1">The 3-2-1 Newsletter</a>
+              <a href="https://jamesclear.com/3-2-1">The 3-2-1 Newsletter</a>.
             </p>
-            <p>“The most wisdom per word of any newsletter on the web.”</p>
           </div>
-        </header>
+        </Header>
         <Quote />
       </ThemeProvider>
     </>
