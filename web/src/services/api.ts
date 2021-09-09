2467e2c1cf9d81b34414849292b6602721e82a7c
Mauricio Robayo <rfmajo@gmail.com>
2021-09-09 11:42:03 -0500

    feat: add data type to api response
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,4 +1,8 @@
-export async function getQuote() {
+export async function getQuote(): Promise<{
+  cttId: string;
+  text: string;
+  source: string;
+}> {
   const response = await fetch("/api/random");
   if (!response.ok) {
     throw new Error(`Request failed! Response code ${response.status}`);
